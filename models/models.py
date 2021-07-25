d5767d96b20712251d529a574f948b82fc9cb571
Nasr Omar <nasrom9@gmail.com>
2021-07-25 12:22:42 +0200

    added some fields in the main model

--- models/models.py
+++ models/models.py
@@ -3,14 +3,14 @@
 # from odoo import models, fields, api
 
 
-# class gold_shop(models.Model):
-#     _name = 'gold_shop.gold_shop'
-#     _description = 'gold_shop.gold_shop'
+class gold_shop(models.Model):
+    _name = 'gold_shop.gold_shop'
+    _description = 'gold_shop.gold_shop'
 #added another comment
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
+    name = fields.Char()
+    value = fields.Integer()
+    value2 = fields.Float(compute="_value_pc", store=True)
+    description = fields.Text()
 #
 #     @api.depends('value')
 #     def _value_pc(self):
